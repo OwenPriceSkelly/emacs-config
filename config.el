6da98cf5185536d8b848ed2b6b222d02aa8d4699
Owen Price <PHOENIX\owen.price@a-2o60fdu0fylom.phoenix.local>
2020-03-25 15:36:50 +1100

    persist frame size defun

--- config.el
+++ config.el
@@ -184,26 +184,27 @@
               lsp-ui-doc-delay 0.4)))))
 
 ;; persist frame size/fullscreen across sessions
-(when-let (dims (doom-cache-get 'last-frame-size))
-  (cl-destructuring-bind ((left . top) width height fullscreen) dims
-    (setq initial-frame-alist
-          (append initial-frame-alist
-                  `((left . ,left)
-                    (top . ,top)
-                    (width . ,width)
-                    (height . ,height)
-                    (fullscreen . ,fullscreen))))))
+(defun +my/persist-frame-size ()
+  (when-let (dims (doom-cache-get 'last-frame-size))
+    (cl-destructuring-bind ((left . top) width height fullscreen) dims
+      (setq initial-frame-alist
+            (append initial-frame-alist
+                    `((left . ,left)
+                      (top . ,top)
+                      (width . ,width)
+                      (height . ,height)
+                      (fullscreen . ,fullscreen))))))
+  (defun save-frame-dimensions ()
+    (doom-cache-set 'last-frame-size
+                    (list (frame-position)
+                          (frame-width)
+                          (frame-height)
+                          (frame-parameter nil 'fullscreen))))
+  (add-hook 'kill-emacs-hook  #'save-frame-dimensions))
 
-(defun save-frame-dimensions ()
-  (doom-cache-set 'last-frame-size
-                  (list (frame-position)
-                        (frame-width)
-                        (frame-height)
-                        (frame-parameter nil 'fullscreen))))
-
-(add-hook 'kill-emacs-hook  #'save-frame-dimensions)
 (general-auto-unbind-keys)
 
+(+my/persist-frame-size)
 (+my/variables)
 (+my/use-packages)
 (+my/keybindings)
