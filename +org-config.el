28f7cfce9005e7e1587be8d6eb02b600306c7d57
Owen Price <PHOENIX\owen.price@a-2o60fdu0fylom.phoenix.local>
2020-04-01 16:10:00 +1100

    major config refactoring

---
+++ +org-config.el
@@ -0,0 +1,71 @@
+(use-package! org
+  :init
+  (setq org-directory                   "~/.org.d/")
+  :config
+  (setq org-startup-folded              'content
+          ;; org-preview-latex-default-process 'dvipng
+          org-startup-with-latex-preview t
+          org-preview-latex-default-process 'dvisvgm
+          ;; org-preview-latex-default-process 'imagemagick
+          org-format-latex-options '(:foreground default :background default :scale 1.0
+                                                :html-foreground "Black" :html-background "Transparent"
+                                                :html-scale 1.0 :matchers ("begin" "$1" "$" "$$" "\\(" "\\["))
+          org-agenda-files '("~/.org.d/")
+          org-bullets-bullet-list '( "▶" "◉" "▸" "○" "✸" "•" "★")
+          org-startup-folded 'content
+          org-todo-keywords '((sequence "[ ](t)"     ; A subtask
+                                        "[~](p)"     ; Subtask currently in-progress
+                                        "[*](w!)"    ; Subtask is being held up or paused
+                                        "|"
+                                        "[X](d!)"    ; Subtask was completed ; ; ;
+                                        "[-](k@)")   ; Subtask was dropped
+                              (sequence "TODO(T)"    ; A task that needs doing & is ready to do
+                                        "PROG(P)"    ; Mark a task as in-progress
+                                        "WAIT(W!)"   ; Something is holding up this task or it is paused
+                                        "|"
+                                        "DONE(D!)"   ; Task successfully completed
+                                        "DROP(K@)")) ; Task was cancelled or is no longer applicable
+          org-todo-keyword-faces '(("[~]"   . +org-todo-active)
+                                   ("[*]"   . +org-todo-onhold)
+                                   ("PROG"  . +org-todo-active)
+                                   ("WAIT"  . +org-todo-onhold))))
+
+
+(use-package! org-roam
+  :defer-incrementally org
+  :commands
+  (org-roam org-roam-find-file
+            org-roam-insert
+            org-roam-graph-show
+            org-roam-today
+            org-roam-tomorrow
+            org-roam-yesterday
+            org-roam-date)
+  :config
+  (setq org-roam-directory              org-directory
+        org-roam-db-location            (concat       org-roam-directory "org-roam.db")
+        org-roam-buffer-width           0.30
+        org-roam-completion-fuzzy-match t
+        org-roam-date-filename-format   "%Y-%m-%d-%A"   ; YYYY-mm-dd-Weekday
+        org-roam-date-title-format      "%A, %B %d, %Y" ; Weekday, Month dd, YYYY
+        org-roam-graph-max-title-length 50
+        org-roam-buffer-position        'right
+        org-roam-graph-viewer           (if IS-MAC "open"
+                                          "firefox")
+        org-roam-graph-exclude-matcher  "old/"
+        org-roam-capture-templates      '(("d" "default" entry (function org-roam-capture--get-point)
+                                           "%?"
+                                           :file-name "%<%Y-%m-%d>-${slug}"
+                                           :head "#+TITLE: ${title}\n* Tags:\n** From: [[file:%F][${captured-from}]]\n** Category: \n* Description: "
+                                           :unnarrowed t))
+        org-capture-templates           org-roam-capture-templates)
+  (org-roam-mode +1))
+
+(use-package! org-roam-protocol
+  :after org-protocol)
+
+(use-package! company-org-roam
+  :when (featurep! :completion company)
+  :after org-roam
+  :config
+  (set-company-backend! 'org-mode '(company-org-roam company-yasnippet company-dabbrev)))
--- config.el
+++ config.el
@@ -1,174 +1,47 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
-(defun +my/variables ()
-  (setq user-full-name "Owen Price-Skelly"
-        user-mail-address "Owen.Price.Skelly@gmail.com"
+(setq user-full-name "Owen Price-Skelly"
+      user-mail-address "Owen.Price.Skelly@gmail.com"
 
-        doom-theme 'doom-rouge
-        doom-font (font-spec :family (if IS-MAC "Anonymous Pro" "monospace") :size 14)
-        solaire-mode-auto-swap-bg t
-        solaire-mode-remap-line-numbers t
+      doom-theme 'doom-rouge
+      doom-font (font-spec :family (if IS-MAC "Anonymous Pro" "monospace") :size 14)
+      solaire-mode-auto-swap-bg t
+      solaire-mode-remap-line-numbers t
 
-        highlight-indent-guides-method 'character
-        highlight-indent-guides-responsive 'stack
-        highlight-indent-guides-delay 0.2
+      highlight-indent-guides-method 'character
+      highlight-indent-guides-responsive 'stack
+      highlight-indent-guides-delay 0.2
 
 
-        which-key-side-window-location 'bottom
-        which-key-sort-order 'which-key-key-order-alpha
+      which-key-side-window-location 'bottom
+      which-key-sort-order 'which-key-key-order-alpha
 
-        display-line-numbers-type 'relative
+      display-line-numbers-type 'relative
 
-        ranger-override-dired t
+      ranger-override-dired t
 
-        iedit-occurrence-context-lines 1
-        fill-column 88
-        which-key-max-description-length nil
+      iedit-occurrence-context-lines 1
+      fill-column 88
+      which-key-max-description-length nil
 
-        undo-tree-visualizer-diff nil
-        +workspaces-on-switch-project-behavior t
+      undo-tree-visualizer-diff nil
+      +workspaces-on-switch-project-behavior t
 
-        +latex-viewers '(pdf-tools)
+      +latex-viewers '(pdf-tools)
 
-        doom-leader-key "SPC"
-        doom-leader-alt-key "C-SPC"
-        doom-localleader-key ","
-        doom-localleader-alt-key "C-,"))
+      doom-leader-key "SPC"
+      doom-leader-alt-key "C-SPC"
+      doom-localleader-key ","
+      doom-localleader-alt-key "C-,")
 
-(defun +my/org-config ()
-  (setq deft-directory org-directory
-        deft-recursive t
-        deft-use-filter-string-for-filename t
-        deft-default-extension "org"
-        ;; org-preview-latex-default-process 'dvipng
-        org-startup-with-latex-preview t
-        org-preview-latex-default-process 'dvisvgm
-        ;; org-preview-latex-default-process 'imagemagick
-        org-format-latex-options '(:foreground default :background default :scale 1.0
-                                               :html-foreground "Black" :html-background "Transparent"
-                                               :html-scale 1.0 :matchers ("begin" "$1" "$" "$$" "\\(" "\\["))
-        org-directory "~/.org.d/"
-        org-agenda-files '("~/.org.d/")
-        org-bullets-bullet-list '( "▶" "◉" "▸" "○" "✸" "•" "★")
-        org-startup-folded nil
-        org-todo-keywords '((sequence "[ ](t)"     ; A subtask
-                                      "[~](p)"     ; Subtask currently in-progress
-                                      "[*](w!)"    ; Subtask is being held up or paused
-                                      "|"
-                                      "[X](d!)"    ; Subtask was completed ; ; ;
-                                      "[-](k@)")   ; Subtask was dropped
-                            (sequence "TODO(T)"    ; A task that needs doing & is ready to do
-                                      "PROG(P)"    ; Mark a task as in-progress
-                                      "WAIT(W!)"   ; Something is holding up this task or it is paused
-                                      "|"
-                                      "DONE(D!)"   ; Task successfully completed
-                                      "DROP(K@)")) ; Task was cancelled or is no longer applicable
-        org-todo-keyword-faces '(("[~]"   . +org-todo-active)
-                                 ("[*]"   . +org-todo-onhold)
-                                 ;; ("START" . org-agenda-date)
-                                 ("PROG"  . +org-todo-active)
-                                 ("WAIT"  . +org-todo-onhold)))
-  (map! :map org-mode-map
-        :localleader
-        :desc "Sort"     "S"          #'org-sort
-        :desc "preview fragments" "L" #'org-latex-preview))
-;; -----------------------------------------------------------------------------
-;; --------------------------- use-package configs -----------------------------
-;; -----------------------------------------------------------------------------
-(defun +my/use-packages ()
+(use-package! expand-region
+  :config
+  (setq expand-region-contract-fast-key "V"))
 
-  (use-package! expand-region
-    :config
-    (setq expand-region-contract-fast-key "V"))
-  (use-package! evil-textobj-line
-    :demand t))
-(defun +my/treemacs-sidebar ()
-  "Hacky; Found myself selecting window after changing project and needing to
-  close and reopen for treemacs to update to the next project's directory"
-  (interactive)
-  (require 'treemacs)
-  (treemacs-select-window)
-  (+treemacs/toggle)
-  (+treemacs/toggle))
-;; -----------------------------------------------------------------------------
-;; ----------------------------- Keybinding ------------------------------------
-;; -----------------------------------------------------------------------------
-(defun +my/keybindings ()
-  ;; Leader-key bindings
-  (map! :leader
-        :desc "Ivy M-x"                "SPC"           #'counsel-M-x
-        :desc "M-x"                    ":"             #'execute-extended-command
-        :desc "Search project"         "/"             #'+default/search-project
-        :desc "Visual expand"          "v"             #'er/expand-region
-        :desc "Project sidebar"        "0"             #'+my/treemacs-sidebar
-        :desc "Undo Tree"              "U"             #'undo-tree-visualize
+(use-package! evil-textobj-line
+  :demand t)
 
-        (:when (featurep! :ui workspaces)
-          (:prefix "TAB"
-            :desc "Switch workspace"   "TAB"           #'+workspace/switch-to))
 
-        (:when (featurep! :completion company)
-          (:prefix "i"
-            :desc "Show Completions"   "i"             #'+company/complete))
-
-        (:prefix ("n" . "notes")
-          :desc "roam buffer"        "r"   #'org-roam
-          :desc "find/new"           "f"   #'org-roam-find-file
-          :desc "find/new"           "n"   #'org-roam-find-file
-          :desc "insert/new"         "i"   #'org-roam-insert
-          :desc "graph"              "g"   #'org-roam-graph-show
-          :desc "today's file"       "t"   #'org-roam-today
-          :desc "tomorrow's file"    "T"   #'org-roam-tomorrow
-          :desc "yesterday's file"   "y"   #'org-roam-yesterday
-          :desc "<date>'s file"      "d"   #'org-roam-date)
-
-        (:prefix "w"
-          "w"           #'other-window)
-
-        (:prefix "b"
-          :desc "Fallback buffer"        "h"           #'+doom-dashboard/open
-          :desc "Messages buffer"        "m"           #'view-echo-area-messages
-          :desc "ibuffer (other window)" "I"           #'ibuffer-other-window))
-
-  ;; Search/replace bindings
-  (map! :map ivy-minibuffer-map
-        (:prefix "C-c"
-          :desc "Edit and replace"              "e"    #'+ivy/woccur))
-
-  ;; evil-multiedit
-  (map! :nv "R"                                        #'evil-multiedit-match-all
-        :n "C-n"                                       #'evil-multiedit-match-symbol-and-next
-        :n "C-S-n"                                     #'evil-multiedit-match-symbol-and-prev
-        :v "C-n"                                       #'evil-multiedit-match-and-next
-        :v "C-S-n"                                     #'evil-multiedit-match-and-prev
-        :nv "C-M-n"                                    #'evil-multiedit-restore
-        (:after evil-multiedit
-          (:map evil-multiedit-state-map
-            "n"                                        #'evil-multiedit-next
-            "N"                                        #'evil-multiedit-prev
-            "C-n"                                      #'evil-multiedit-match-and-next
-            "C-S-n"                                    #'evil-multiedit-match-and-prev
-            "V"                                        #'iedit-show/hide-unmatched-lines)))
-
-  (map! :m  "SPC j"                                    #'+evil/easymotion))
-
-;; major mode bindings
-(defun +my/python-config ()
-  (setq python-fill-docstring-style 'django)
-  (map! :map python-mode-map
-        :localleader
-        (:prefix ("p" . "pipenv")
-          :desc "activate"    "a" #'pipenv-activate
-          :desc "deactivate"  "d" #'pipenv-deactivate
-          :desc "install"     "i" #'pipenv-install
-          :desc "lock"        "l" #'pipenv-lock
-          :desc "open module" "o" #'pipenv-open
-          :desc "run"         "r" #'pipenv-run
-          :desc "shell"       "s" #'pipenv-shell
-          :desc "uninstall"   "u" #'pipenv-uninstall)
-        (:prefix ("r" . "repl")
-          :desc "default"              "r"              #'+python/open-repl
-          :desc "ipython"              "i"              #'+python/open-ipython-repl
-          :desc "jupyter"              "j"              #'+python/open-jupyter-repl))
+(defun +python-lsp-config ()
   (when (and (featurep! :tools lsp)
              (featurep! :lang python +lsp))
     (setq lsp-pyls-plugins-pylint-enabled t
@@ -176,44 +49,32 @@
           lsp-pyls-plugins-pycodestyle-enabled nil
           lsp-pyls-plugins-flake8-enabled nil
           lsp-pyls-plugins-pyflakes-enabled nil)
-
     (when (featurep! :tools lsp +peek)
-      (map! :map lsp-ui-peek-mode-map
-            "C-j" #'lsp-ui-peek--select-next
-            "C-h" #'lsp-ui-peek--select-prev-file
-            "C-l" #'lsp-ui-peek--select-next-file
-            "C-k" #'lsp-ui-peek--select-prev)
       (after! lsp-ui
         (lsp-ui-doc-enable t)
         (setq lsp-ui-doc-position 'top
               lsp-ui-doc-max-height 35
               lsp-ui-doc-max-width 35
               lsp-ui-doc-delay 0.4)))))
+
+(after! python (+python-lsp-config))
+(after! org (load! "+org-config.el"))
+(load! "+keybindings")
+
 ;; persist frame size/fullscreen across sessions
-(defun +my/persist-frame-size ()
-  (when-let (dims (doom-cache-get 'last-frame-size))
-    (cl-destructuring-bind ((left . top) width height fullscreen) dims
-      (setq initial-frame-alist
-            (append initial-frame-alist
-                    `((left . ,left)
-                      (top . ,top)
-                      (width . ,width)
-                      (height . ,height)
-                      (fullscreen . ,fullscreen))))))
-  (defun save-frame-dimensions ()
-    (doom-cache-set 'last-frame-size
-                    (list (frame-position)
-                          (frame-width)
-                          (frame-height)
-                          (frame-parameter nil 'fullscreen))))
-  (add-hook 'kill-emacs-hook  #'save-frame-dimensions))
-
-(general-auto-unbind-keys)
-
-(after! python (+my/python-config))
-(after! org (+my/org-config))
-
-(+my/persist-frame-size)
-(+my/variables)
-(+my/use-packages)
-(+my/keybindings)
+(when-let (dims (doom-cache-get 'last-frame-size))
+  (cl-destructuring-bind ((left . top) width height fullscreen) dims
+    (setq initial-frame-alist
+          (append initial-frame-alist
+                  `((left . ,left)
+                    (top . ,top)
+                    (width . ,width)
+                    (height . ,height)
+                    (fullscreen . ,fullscreen))))))
+(defun save-frame-dimensions ()
+  (doom-cache-set 'last-frame-size
+                  (list (frame-position)
+                        (frame-width)
+                        (frame-height)
+                        (frame-parameter nil 'fullscreen))))
+(add-hook 'kill-emacs-hook  #'save-frame-dimensions)
