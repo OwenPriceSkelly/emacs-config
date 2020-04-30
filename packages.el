891b908dcefad160929a77e1e9412f80f077056a
Owen Price-Skelly <OwenPriceSkelly@users.noreply.github.com>
2020-04-30 18:08:39 +1000

    [todo] pin packages

--- packages.el
+++ packages.el
@@ -1,18 +1,10 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
-;; (package! hercules)
-(package! evil-textobj-line)
-(package! doom-themes
+(package! evil-textobj-line
   :pin nil)
-;; (package! company-org-roam
-;;   :recipe (:host github :repo "jethrokuan/company-org-roam")
-;;   :pin nil)
+(package! doom-themes :pin nil)
 (package! mathpix.el
-  :recipe (:host github :repo "jethrokuan/mathpix.el"))
-;; (package! org-superstar
-;;   :recipe (:host github :repo "integral-dw/org-superstar-mode"))
-;; (package! org-pdftools
-;;   :recipe (:host github :repo "fuxialexander/org-pdftools"))
-(package! rainbow-mode)
-;; (package! org-roam
-;;   :pin "e33c1442")
+  :recipe (:host github :repo "jethrokuan/mathpix.el")
+  :pin nil)
+(package! rainbow-mode
+  :pin nil)
